6bae29fe39d26fe81c51cf4c2feb35a3f945a03a
Sendoh24 <43398136+Sendoh24@users.noreply.github.com>
2022-05-30 19:13:01 +0800

    Create part3.h

---
+++ wangxy/HW3/part3.h
@@ -0,0 +1,14 @@
+/*
+ * @author WangXingyu
+ * @date 2022/5/30
+ */
+
+#pragma once
+
+typedef int elemtype;  // 特征矩阵元素类型, 后期可以修改
+
+// 读取singles集合, 生成特征矩阵
+elemtype **generateFeatureMatrix(const char *filename, int &feature_dim);
+
+// 处理特征矩阵, 生成签名矩阵
+int **generateSignatureMatrix(elemtype **feature_matrix, int feature_dim);
